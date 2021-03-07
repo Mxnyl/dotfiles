c200106c9887d5d9fa1bf891025d158028ff24dd
Mxnyl <73441963+Mxnyl@users.noreply.github.com>
2021-03-07 19:14:12 +0100

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,14 +1,10 @@
-
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+killall polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar example &
-polybar example2 &
-
-echo "Polybar launched..."
+# Launch polybar
+polybar main -c $(dirname $0)/config.ini &
